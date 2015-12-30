eed48bef60057c21ccec2aaf0d9fce190217f3ce
villavong <asyao@naver.com>
2015-12-30 20:28:30 +0900

    Postgresql Search

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,6 @@
-require 'elasticsearch/model'
 class Student < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
@@ -11,22 +8,40 @@ class Student < ActiveRecord::Base
    has_attached_file :photo, styles: { :large => "1000x1000#", :medium => "550x550#" }
    validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
-   searchkick
 
+  #  acts_as_taggable
 
+   def self.search(query)
+       if query.present?
+         # where("name like :q", q: "%#{query}%")
+         # where("name ilike :q or description ilike :q or ship ilike :q", q: "%#{query}%")
+         # where("name @@ :q or description @@ :q or ship @@ :q", q: query)
+        #  rank = <<-RANK
+          #  ts_rank(to_tsvector(name), plainto_tsquery(#{sanitize(query)})) +
+          #  ts_rank(to_tsvector(school), plainto_tsquery(#{sanitize(query)})) +
+          #  ts_rank(to_tsvector(city), plainto_tsquery(#{sanitize(query)})) +
+          #  ts_rank(to_tsvector(major), plainto_tsquery(#{sanitize(query)})) +
+          #  ts_rank(to_tsvector(nationality), plainto_tsquery(#{sanitize(query)})) +
+          #  ts_rank(to_tsvector(email), plainto_tsquery(#{sanitize(query)})) +
+          #  ts_rank(to_tsvector(introduction), plainto_tsquery(#{sanitize(query)}))
+        #  RANK
+         where("name @@ :q or school @@ :q or city @@ :q or major @@ :q or nationality @@ :q or introduction @@ :q or email @@ :q", q: query)
+       else
+         all
+       end
+     end
 
-
-   def search_data
-     {
-       name: name,
-       email: email,
-       school: school,
-       introduction: introduction,
-       city: city,
-       major: major,
-       nationality: nationality
-     }
-   end
+  #  def search_data
+  #    {
+  #      name: name,
+  #      email: email,
+  #      school: school,
+  #      introduction: introduction,
+  #      city: city,
+  #      major: major,
+  #      nationality: nationality
+  #    }
+  #  end
 
 
    def birdtype
@@ -46,4 +61,3 @@ class Student < ActiveRecord::Base
 
 
 end
-Student.import
