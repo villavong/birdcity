35fec19abcfc3a9f125be62dd08720a377e27144
villavong <asyao@naver.com>
2015-12-30 15:37:41 +0900

    Created Devise Student

---
+++ db/migrate/20151230063715_add_attributes_to_student.rb
@@ -0,0 +1,10 @@
+class AddAttributesToStudent < ActiveRecord::Migration
+  def change
+    add_column :students, :name, :string
+    add_column :students, :school, :string
+    add_column :students, :major, :string
+    add_column :students, :city, :string
+    add_column :students, :nationality, :string
+    add_column :students, :introduction, :text
+  end
+end
