ff7fcbdde1ac660a6a2f54703746fc0336c0d514
villavong <asyao@naver.com>
2015-12-31 14:29:55 +0900

    paperclip beppupost.rb photo add .rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230161108) do
+ActiveRecord::Schema.define(version: 20151231045455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,8 +31,12 @@ ActiveRecord::Schema.define(version: 20151230161108) do
     t.string   "title"
     t.text     "content"
     t.integer  "student_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   add_index "beppuposts", ["student_id"], name: "index_beppuposts_on_student_id", using: :btree
