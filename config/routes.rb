f3207f87c2d1a38cc04d0be2e33dfb0b94b18f7d
villavong <asyao@naver.com>
2016-01-01 17:18:20 +0900

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :students
+  devise_for :students, controllers: { registrations: "registrations" }
     root 'welcome#index'
 
   resources :students
