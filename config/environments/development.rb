8321fa243119e4aca7c8881a15a7165645b637b7
villavong <asyao@naver.com>
2016-01-01 14:07:32 +0900

    development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,8 +35,18 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { :host => 'quiet-badlands-3939.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address            => 'smtp.sendgrid.net',
+    :port               => '587',
+    :authentication     => :plain,
+    :user_name          => 'app45626912@heroku.com',
+    :passwords          => '9a4luqmz5298',
+    :domain             => 'heroku.com',
+    :enable_starttls_auto => true
+  }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
