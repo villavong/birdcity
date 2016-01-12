ab16dbde95871c8da922264317a88860669f6903
villavong <asyao@naver.com>
2016-01-13 00:04:29 +0900

    undefinedmethod - 'add that action in model'

--- app/models/fukuokapost.rb
+++ app/models/fukuokapost.rb
@@ -1,3 +1,7 @@
 class Fukuokapost < ActiveRecord::Base
   belongs_to :student
+  has_many :fukuokacomments, :dependent => :delete_all
+
+  has_attached_file :photo, styles: { :large => "1000x1000#", :medium => "550x550#" }
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
