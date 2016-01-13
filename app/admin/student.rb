478b33a3385b6e7a7b9c5ef3576bd9eeb5643fef
villavong <asyao@naver.com>
2016-01-13 14:48:09 +0900

    student.rb admin edit

--- app/admin/student.rb
+++ app/admin/student.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Student do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :photo, :city, :nationality, :school, :name, :introduction, :email
 #
 # or
 #
