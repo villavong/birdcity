4ac6362ea006623fb7a040c2ec47c17a52067031
villavong <asyao@naver.com>
2016-01-22 13:28:02 +0900

    update

--- app/controllers/registrations_contoroller.rb
+++ app/controllers/registrations_contoroller.rb
@@ -9,9 +9,12 @@ class RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.requrie(:student).permit(:email, :password, :password_confirmation, :current_password, :name, :school, :major, :city, :nationality)
   end
+
   protected
 
-   def update_resource(resource, params)
-     resource.update_without_password(params)
-   end
+  def update_resource(resource, params)
+   binding.pry
+   resource.update_without_password(params)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
 
-  devise_for :students, controllers: {registrations: 'students/registrations'}
+  devise_for :students, controllers: {registrations: 'registrations'}
 
 
 
