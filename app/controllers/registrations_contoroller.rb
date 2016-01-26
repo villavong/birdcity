4fb45399fcb3927c5556797918ee18a1f6f7ae3f
villavong <asyao@naver.com>
2016-01-26 19:33:18 +0900

    registrations controller

--- app/controllers/registrations_contoroller.rb
+++ app/controllers/registrations_contoroller.rb
@@ -3,11 +3,11 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:student).permit(:email, :password, :password_confirmation, :name, :school, :major, :city, :nationality)
+    params.require(:student).permit(:email, :password, :password_confirmation, :name, :school, :major, :city, :nationality, :photo, :work, :pay, :gift, :mission)
   end
 
   def account_update_params
-    params.requrie(:student).permit(:email, :password, :password_confirmation, :current_password, :name, :school, :major, :city, :nationality)
+    params.requrie(:student).permit(:email, :password, :password_confirmation, :current_password, :name, :school, :major, :city, :nationality, :photo, :work, :pay, :gift, :mission)
   end
 
 
