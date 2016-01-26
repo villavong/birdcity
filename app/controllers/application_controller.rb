45f80919f07bec2eda736a1088053c148a62d1f7
villavong <asyao@naver.com>
2016-01-26 19:55:08 +0900

    logs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :name, :school, :major, :city, :introduction, :nationality, :photo, :work, :pay, :gift, :missions)}
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :name, :school, :major, :city, :introduction, :nationality, :photo, :work, :pay, :gift, :missions)}
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :name, :school, :major, :city, :introduction, :nationality, :photo, :work, :pay, :gift, :mission)}
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :name, :school, :major, :city, :introduction, :nationality, :photo, :work, :pay, :gift, :mission)}
   end
 
 end
