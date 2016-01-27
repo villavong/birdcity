31ff968db13063b0d5ad1a43215b562812221055
villavong <asyao@naver.com>
2016-01-27 18:44:53 +0900

    photo

--- app/models/studentcomment.rb
+++ app/models/studentcomment.rb
@@ -2,8 +2,6 @@ class Studentcomment < ActiveRecord::Base
   belongs_to :student
 
   has_attached_file :photo, styles: { :large => "1000x1000#", :medium => "550x550#" }
-
-
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
 end
