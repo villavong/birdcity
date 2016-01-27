5ffb237e0c5531457dee7269b1105cfa0722dfef
villavong <asyao@naver.com>
2016-01-27 18:02:23 +0900

    controlelr

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,59 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_filter :require_permission, only: [:edit, :update, :destroy]
+  before_action :authenticate_student!, only: [:new, :create]
 
-  
+  def show
+  end
+
+  def new
+    @post = current_student.posts.build
+  end
+  def create
+    @post = current_student.posts.build(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+  def edit
+  end
+
+  def update
+    if @post.update (post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+
+  end
+
+
+  def destroy
+    @post.destroy
+    redirect_to student_posts_path
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+
+  def post_params
+    params.require(:post).permit(:title, :content, :photo)
+  end
+
+  def require_permission
+    @post = Post.find(params[:id])
+    if current_student.id != @post.student_id
+      redirect_to root_path, notice: "Sorry, you're not allowed"
+    end
+
+  end
+
+
 end
