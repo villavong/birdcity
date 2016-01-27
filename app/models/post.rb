ea6db912b8729f6c759bbba8680c108fb40e9475
villavong <asyao@naver.com>
2016-01-27 23:29:54 +0900

    news3 rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :student
+  has_many :comments
 
   has_attached_file :photo, styles: { :large => "1000x1000#", :medium => "550x550#" }
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
