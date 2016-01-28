8b4e87eeeb8dbcd3b48fc00d42e9f6686cc6916e
villavong <asyao@naver.com>
2016-01-28 14:09:35 +0900

    posts photo rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,8 @@ class Post < ActiveRecord::Base
   has_many :comments
 
   has_attached_file :photo, styles: { :large => "1000x1000#", :medium => "550x550#" }
-  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/ => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
+
 
   has_attached_file :mp3
   validates_attachment_content_type :mp3, :content_type => { :content_type => ["audio/mpeg", "audio/mp3"]}, :file_name => { :matches => [/\mp3\Z/]}
